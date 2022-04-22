0867803551a298686ee7a1fcce4f4d9c038287de
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-04-22 20:18:41 -0300

    promise

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,43 +1,30 @@
-import React from 'react';
-import Cards from '../Cards/Cards';
+import React, { useEffect, useState } from 'react';
+import ItemList from '../ItemList/ItemList';
+import Productos from '../Productos/Productos';
 import './ItemListContainer.css'
 
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = () => {
+    const [item, setItem] = useState([])
 
-    const productos = [
-        {id: "001",
-        img: "http://fronteraliving.com/imagenes/productos/gfr-193-277954.jpg",
-        nombre: "Rinconero Palermo",
-        precio:"$150000",
-        stock: 5,
-        quantity: 0},
-        {id: "002",
-        img: "http://fronteraliving.com/imagenes/productos/gfr-65-673404.jpg",
-        nombre: "Rinconero Zaro",
-        precio: "$130000",
-        stock: 7,
-        quantity: 0},
-        {id: "003",
-        img: "http://fronteraliving.com/imagenes/productos/gfr-177-527065.jpg",
-        nombre: "Rinconero Genesis",
-        precio: "$160000",
-        stock: 8,
-        quantity: 0},
-        {id: "004",
-        img: "http://fronteraliving.com/imagenes/productos/gfr-62-199060.jpg",
-        nombre: "Rinconero Dubai",
-        precio: "$90000",
-        stock: 3,
-        quantity: 0}
-    ];
+    function traeProductos() {
+        const myPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(Productos)
+            }, 2000)
+        });
+        return myPromise
+    };
+    
+    useEffect(() => {
+        traeProductos()
+            .then(res => setItem(res))
+            .catch(err => console.log(`error en promise`))
+      },[]);
+
 
     return (
         <>
-            <h3>{greeting}</h3>
-            <section className='menu'>
-                {productos.map((item, index) =>
-                    <Cards key={index} producto={item}/>)}
-            </section>
+            <ItemList item={item}/>
         </>
     );
 };
