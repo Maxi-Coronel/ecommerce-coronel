8713030a545a4bc09ec46df0838be3c7e7eeb7c2
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-04-22 20:31:48 -0300

    simplification itemcount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '../Button/Button';
 import './ItemCount.css'
 
-const ItemCount = ({producto}) => {
-    const {stock} = producto;
-
-    const [count, setCount] = useState(0)
-
-    function handlerCart() {
-        console.log(producto);
-        producto.quantity = producto.quantity + count;
-        count + producto.quantity > stock && setCount(stock - producto.quantity)
-    }
+const ItemCount = ({maximo, count, funcion, cantidad}) => {
 
     function handlerAdd() {
-        stock > count+producto.quantity && setCount(count+1)
+        cantidad?
+        maximo > count + cantidad && funcion(count+1) :
+        maximo > count && funcion(count+1)
     }
 
     function handlerSubtract() {
-       count > 0 && setCount(count-1)
+       count > 1 && funcion(count-1)
     }
 
     return (
@@ -28,7 +21,6 @@ const ItemCount = ({producto}) => {
                 <h3>{count}</h3>
                 <Button functional={handlerAdd} nombre={"+"}/>
             </div>
-            <Button functional={handlerCart} nombre={"Agregar al carrito"}/>
         </div>
     );
 };
