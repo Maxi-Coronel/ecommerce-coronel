adf5db32e05c23911200ec4f4a91b5d6e35843ad
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-04-22 20:30:58 -0300

    products

---
+++ src/components/Productos/Productos.jsx
@@ -0,0 +1,28 @@
+const Productos = [
+    {id: "001",
+    img: "http://fronteraliving.com/imagenes/productos/gfr-193-277954.jpg",
+    nombre: "Rinconero Palermo",
+    precio:150000,
+    stock: 5,
+    quantity: 0},
+    {id: "002",
+    img: "http://fronteraliving.com/imagenes/productos/gfr-65-673404.jpg",
+    nombre: "Rinconero Zaro",
+    precio: 130000,
+    stock: 7,
+    quantity: 0},
+    {id: "003",
+    img: "http://fronteraliving.com/imagenes/productos/gfr-177-527065.jpg",
+    nombre: "Rinconero Genesis",
+    precio: 160000,
+    stock: 8,
+    quantity: 0},
+    {id: "004",
+    img: "http://fronteraliving.com/imagenes/productos/gfr-62-199060.jpg",
+    nombre: "Rinconero Dubai",
+    precio: 90000,
+    stock: 3,
+    quantity: 0}
+];
+
+export default Productos;
