fc6497d8049d23c92be1f309174c0f978942798b
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-04-23 08:42:48 -0300

    handlerCuotas

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -14,16 +14,13 @@ const Item = ({producto}) => {
         setPrecioCuotas(countCuotas > 1 ? (((precio * (0.05 * countCuotas) + precio) / countCuotas) * count) : precio)
       },[countCuotas, count]);
 
-    function modificarCount(a) {
-        setCount(a)
-    }
+    const handlerCount = a => setCount(a)
 
-    function cuotas() {
+    const handlerCuotas = () => {
         setCountCuotas(document.getElementById(id).value)
-        console.log(document.getElementById(id).value);
     }
 
-    function handlerCart() {
+    const handlerCart = () => {
         console.log(producto);
         producto.quantity = producto.quantity + count;
         count + producto.quantity > producto.stock && setCount(producto.stock - producto.quantity)
@@ -36,7 +33,7 @@ const Item = ({producto}) => {
                 <h3>{nombre}</h3>
                 <h4>${precio}</h4>
                 <div>
-                    <select name="" id={id} onChange={cuotas}>
+                    <select name="numCoutas" id={id} onChange={handlerCuotas}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="6">6</option>
@@ -46,7 +43,7 @@ const Item = ({producto}) => {
                     <p>cuotas de ${precioCuotas.toFixed(2)}</p>
                 </div>
             </div>
-            <ItemCount maximo={stock} count={count} funcion={modificarCount} cantidad={quantity} />
+            <ItemCount maximo={stock} cantidad={quantity} count={count} funcion={handlerCount} />
             <Button functional={handlerCart} nombre={"Agregar al carrito"}/>
         </article>
     );
