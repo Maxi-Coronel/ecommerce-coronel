681ed834397504794020732d78437e176f66e152
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-04-26 18:50:33 -0300

    translate item to english

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,18 +3,18 @@ import ItemCount from '../ItemCount/ItemCount';
 import './Item.css'
 
 const Item = ({producto}) => {
-    const {nombre, img, precio, stock} = producto;
+    const {id, title, description, pictureUrl, price, stock} = producto;
 
     const handlerCart = (count) => {
-        alert(`Agregaste ${count} ${nombre} al carrito`)
+        alert(`Agregaste ${count} ${title} al carrito`)
     }
 
     return (
         <article className='card'>
-            <img src={img} alt={nombre} />
+            <img src={pictureUrl} alt={title} />
             <div>
-                <h3>{nombre}</h3>
-                <h4>${precio}</h4>
+                <h3>{title}</h3>
+                <h4>${price}</h4>
             </div>
             <ItemCount stock={stock} initial={1} onAdd={handlerCart} />
         </article>
