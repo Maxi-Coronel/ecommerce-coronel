c907a44cc30c2f663ff92528d572bf68737f0dea
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-05-03 19:26:03 -0300

    fixes category parameter

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path='/' element={<ItemListContainer />}/>
-        <Route path='/category/:id' element={<ItemListContainer />}/>
+        <Route path='/category/:categoryId' element={<ItemListContainer />}/>
         <Route path='/item/:id' element={<ItemDetailContainer />}/>
       </Routes>
     </div>
