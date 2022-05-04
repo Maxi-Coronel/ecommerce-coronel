0a8f48b074dd201d1b3511419cf996ecd1855861
Maximiliano Coronel <maxicoronel1992@gmail.com>
2022-05-04 20:59:40 -0300

    adds interchangeable component

--- src/components/ItemListContainer/ItemList/Item/Item.jsx
+++ src/components/ItemListContainer/ItemList/Item/Item.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../../../Button/Button';
 import ItemCount from '../../../ItemCount/ItemCount';
 import './Item.css';
 
 const Item = ({ item }) => {
     const {id, title, pictureUrl, price, stock} = item;
-
+    const [quantityToAdd, setquantityToAdd] = useState(0)
+    
     const handlerCart = (count) => {
         alert(`Agregaste ${count} ${title} al carrito`);
+        setquantityToAdd(count);
     }
 
     return (
@@ -19,7 +22,10 @@ const Item = ({ item }) => {
                     <h4>${price}</h4>
                 </div>
             </Link>
-            <ItemCount stock={stock} initial={1} onAdd={handlerCart} />
+            {quantityToAdd<1 ? 
+                <ItemCount stock={stock} initial={1} onAdd={handlerCart}/> : 
+                <Link to={"/cart"}><Button content="Terminar mi compra"/></Link>
+            }
         </div>
     );
 };
